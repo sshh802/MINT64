bc23563fdd033ec8964c91e29ac29abf5ca0a30c
sshh802 <sshh802@naver.com>
2021-03-18 07:33:25 +0000

    chap08

--- 01.Kernel32/Source/Main.c
+++ 01.Kernel32/Source/Main.c
@@ -1,10 +1,36 @@
 #include "Types.h"
 
 void kPrintString(int iX, int iY, const char* pcString);
+BOOL kInitializeKernel64Area(void);
+BOOL kIsMemoryEnough(void);
 
 void Main(void)
 {
-	kPrintString(0,3,"C Language Kernel Started!");
+	DWORD i;
+
+	kPrintString(0,3,"C Language Kernel Started!................[Pass]");
+
+	kPrintString(0, 4, "Minimum Memory Size Check.................[    ]");
+	if(kIsMemoryEnough()==FALSE)
+	{
+		kPrintString(45,4, "Fail");
+		kPrintString(0, 5, "Not Enouth Memory MINT64 OS Required Over "
+			"64Mbyte Memory!");
+		while(1);	
+	}
+	else
+	{
+		kPrintString(45, 4, "Pass");
+	}
+
+	kPrintString(0, 5, "IA-32e Kernel Area Initialize...............[    ]");
+	if(kInitializeKernel64Area()==FALSE)
+	{
+		kPrintString(45,5,"Fail");
+		kPrintString(0, 6, "Kernel Area Initialization Fail");
+		while(1);
+	}
+	kPrintString(45,5, "Pass");
 
 	while(1);
 
@@ -22,3 +48,42 @@ void kPrintString(int iX, int iY, const char* pcString)
 		pstScreen[i].bCharactor=pcString[i];
 	}
 }
+
+BOOL kInitializeKernel64Area(void)
+{
+	DWORD* pdwCurrentAddress;
+
+	pdwCurrentAddress = (DWORD*)0X100000;
+
+	while((DWORD)pdwCurrentAddress < 0x600000)
+	{
+		*pdwCurrentAddress = 0x00;
+
+		if(*pdwCurrentAddress!=0)
+		{
+			return FALSE;
+		}
+		pdwCurrentAddress++;
+	}
+	return TRUE;
+}
+
+BOOL kIsMemoryEnough(void)
+{
+	DWORD* pdwCurrentAddress;
+
+	pdwCurrentAddress = (DWORD*) 0x100000;
+
+	while((DWORD)pdwCurrentAddress < 0x4000000)
+	{
+		*pdwCurrentAddress = 0x12345678;
+		if(*pdwCurrentAddress != 0x12345678)
+		{
+			return FALSE;
+		}
+
+		pdwCurrentAddress += (0x100000/4);
+	}
+	return TRUE;
+}
+
