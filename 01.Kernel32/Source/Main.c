57eccb0a759a56c6a0e4b0846227cffc33e6944b
sshh802 <sshh802@naver.com>
2021-03-25 09:08:36 +0000

    chap10

--- 01.Kernel32/Source/Main.c
+++ 01.Kernel32/Source/Main.c
@@ -1,13 +1,18 @@
 #include "Types.h"
 #include "Page.h"
+#include "ModeSwitch.h"
 
 void kPrintString(int iX, int iY, const char* pcString);
 BOOL kInitializeKernel64Area(void);
 BOOL kIsMemoryEnough(void);
+void kCopyKernel64ImageTo2Mbyte(void);
 
 void Main(void)
 {
 	DWORD i;
+
+	DWORD dwEAX, dwEBX, dwECX, dwEDX;
+	char vcVendorString[13]={0, };
 
 	kPrintString(0,3,"C Language Kernel Started!................[Pass]");
 
@@ -36,6 +41,30 @@ void Main(void)
 	kPrintString(0, 6, "IA-32 Page Tables Initialize................[    ]");
 	kInitializePageTables();
 	kPrintString(45, 6, "Pass");
+	
+	kReadCPUID(0x00, &dwEAX, &dwEBX, &dwECX, &dwEDX);
+	*(DWORD*)vcVendorString = dwEBX;
+	*((DWORD*)vcVendorString +1 ) = dwEDX;
+	*((DWORD*)vcVendorString +2 ) = dwECX;
+	kPrintString(0, 7, "Processor Vendor String.....................[              ]");
+	kPrintString(45, 7, vcVendorString);
+
+	kReadCPUID(0x80000001, &dwEAX, &dwEBX, &dwECX, &dwEDX);
+	kPrintString(0, 8, "64bit Mode Support Check....................[    ]");
+	if(dwEDX & (1 << 29))
+	{
+		kPrintString(45, 8, "Fail");
+		kPrintString(0, 9, "This processor does not support 64bit mode !");
+		while(1);
+	}
+
+	kPrintString(0, 9, "Copy IA-32e Kernel To 2M Address............[    ]");
+	kCopyKernel64ImageTo2Mbyte();
+	kPrintString(45, 9, "Pass");
+
+	kPrintString(0, 10, "Switch To IA-32e Mode");
+	kSwitchAndExecute64bitKernel();
+
 	while(1);
 }
 
@@ -89,4 +118,22 @@ BOOL kIsMemoryEnough(void)
 	}
 	return TRUE;
 }
+void kCopyKernel64ImageTo2Mbyte(void)
+{
+	WORD wKernel32SectorCount, wTotalKernelSectorCount;
+	DWORD* pdwSourceAddress, * pdwDestinationAddress;
+	int i;
 
+	wTotalKernelSectorCount = *((WORD*)0x7C05);
+	wKernel32SectorCount = *((WORD*)0x7C07);
+
+	pdwSourceAddress = (DWORD*)(0x10000 + (wKernel32SectorCount * 512));
+	pdwDestinationAddress = (DWORD*)0x200000;
+
+	for(i=0; i<512*(wTotalKernelSectorCount - wKernel32SectorCount)/4;i++)
+	{
+		*pdwDestinationAddress = *pdwSourceAddress;
+		pdwDestinationAddress++;
+		pdwSourceAddress++;
+	}
+}
--- 04.Utility/00.ImageMaker/ImageMaker.c
+++ 04.Utility/00.ImageMaker/ImageMaker.c
@@ -10,7 +10,7 @@
 #define BYTESOFSECTOR 512
 
 int AdjustInSectorSize(int iFd, int iSourceSize);
-void WriteKernelInformation(int iTargetFd, int iKernelSectorCount);
+void WriteKernelInformation(int iTargetFd, int iKernelSectorCount,int iKernel32SectorCount);
 int CopyFile(int iSourceFd, int iTargetFd);
 
 int main(int argc, char* argv[])
@@ -19,11 +19,12 @@ int main(int argc, char* argv[])
 	int iTargetFd;
 	int iBootLoaderSize;
 	int iKernel32SectorCount;
+	int iKernel64SectorCount;
 	int iSourceSize;
 
-	if(argc <3)
+	if(argc <4)
 	{
-		fprintf(stderr, "[ERROR] ImageMaker BootLoader.bin Kernel32.bin\n");
+		fprintf(stderr, "[ERROR] ImageMaker BootLoader.bin Kernel32.bin Kernel64.bin\n");
 		exit(-1);
 	}
 
@@ -60,8 +61,20 @@ int main(int argc, char* argv[])
 	iKernel32SectorCount = AdjustInSectorSize(iTargetFd, iSourceSize);
 	printf("[INFO] %s size = [%d] and sector count = [%d]\n", argv[2], iSourceSize, iKernel32SectorCount);
 
+	printf("[INFO] Copy IA-32e mode kernel to image file\n");
+	if((iSourceFd = open(argv[3], O_RDONLY)) == -1)
+	{
+		fprintf(stderr, "[ERROR] %s open fail\n", argv[3]);
+		exit(-1);
+	}
+	iSourceSize = CopyFile(iSourceFd, iTargetFd);
+	close(iSourceFd);
+
+	iKernel64SectorCount = AdjustInSectorSize(iTargetFd, iSourceSize);
+	printf("[INFO] %s size = [%d] and sector count = [%d]\n", argv[3], iSourceSize, iKernel64SectorCount);
+
 	printf("[INFO] Start to write kernel information\n");
-	WriteKernelInformation(iTargetFd, iKernel32SectorCount);
+	WriteKernelInformation(iTargetFd, iKernel32SectorCount,iKernel64SectorCount);
 	printf("[INFO] Image file create complete\n");
 
 	close(iTargetFd);
@@ -97,7 +110,7 @@ int AdjustInSectorSize(int iFd, int iSourceSize)
 	return iSectorCount;
 }
 
-void WriteKernelInformation(int iTargetFd, int iKernelSectorCount)
+void WriteKernelInformation(int iTargetFd, int iTotalKernelSectorCount, int iKernel32SectorCount)
 {
 	unsigned short usData;
 	long lPosition;
@@ -109,10 +122,13 @@ void WriteKernelInformation(int iTargetFd, int iKernelSectorCount)
 		exit(-1);
 	}
 
-	usData = (unsigned short)iKernelSectorCount;
+	usData = (unsigned short)iTotalKernelSectorCount;
+	write(iTargetFd, &usData, 2);
+	usData = (unsigned short)iKernel32SectorCount;
 	write(iTargetFd, &usData, 2);
 
-	printf("[INFO] Total sector count execept boot loader [%d]\n", iKernelSectorCount);
+	printf("[INFO] Total sector count execept boot loader [%d]\n", iTotalKernelSectorCount);
+	printf("[INFO] Total sector count of protected mode kernel [%d]\n", iKernel32SectorCount);
 }
 
 int CopyFile(int iSourceFd, int iTargetFd)
