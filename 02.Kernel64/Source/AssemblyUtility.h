cc30bb3773fecc56ef4a4bae19467afc48cf3402
sshh802 <sshh802@naver.com>
2021-04-20 09:09:24 +0000

    chap11:1

---
+++ 02.Kernel64/Source/AssemblyUtility.h
@@ -0,0 +1,9 @@
+#ifndef __ASSEMBLYUTILITY_H__
+#define __ASSEMBLYUTILITY_H__
+
+#include "Types.h"
+
+BYTE kInPortByte(WORD wPort);
+BYTE kOutPortByte(WORD wPort, BYTE bData);
+
+#endif /*__ASSEMBLYUTILITY_H__*/
